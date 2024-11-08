fd8368612a43d5b2d9ad81f592238493909f78df
Vis_maya_ <vismayavinod250@gmail.com>
2024-11-09 02:23:04 +0530

    binary search.c

--- binary search.c
+++ binary search.c
@@ -1,47 +1,53 @@
-#include<stdio.h>
-int  main()
-{
-   int n,x,flag=0,i,t;
-   int right,left=0,mid=0;
-   printf("enter the size of the array; ");t++;
-   scanf("%d",&n);t++;
-   int a[n];
-   printf("\nenter the elements of the array: ");t++;
-   for(i=0;i<n;i++)
-   {
-     t++;t++;
-     scanf("%d",&a[i]);
-    }
-    printf("\nenter the element to be search: ");t++;
-    scanf("%d",&x);t++;
+#include <stdio.h>
+
+int main() {
+    int n, x, flag = 0, i;
+    int left = 0, right, mid;
     
-    while(left<=right)
-    {
-    mid=(left+right)/2;
-    t++;t++;
-    if(x==a[mid])
-    {
-     flag=1;t++;
-     break;
-    }
-     else if(x>a[mid])
-    {
-     left=mid+1;t++;
-    }
-     else if(x<a[mid])
-    {
-     right=mid-1;
+    // Get the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    
+    int a[n]; // Dynamic array allocation
+    
+    // Get the elements of the array
+    printf("\nEnter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
     
-    if(flag==0)
-    {
-     printf("element not found");t++;
-     }
-    else if(flag==1)
-    {
-     printf("element found");t++;
+    // Get the element to be searched
+    printf("\nEnter the element to be searched: ");
+    scanf("%d", &x);
+    
+    right = n - 1;  // Set the right boundary for binary search
+    
+    // Perform binary search
+    while (left <= right) {
+        mid = (left + right) / 2;
+        
+        if (x == a[mid]) {
+            flag = 1;
+            break;
+        } else if (x > a[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    printf("\nspace complexity%d",(4*8)+4*n);t+2;
-    printf("\ntime com[lexity%d",t);
+    
+    // Output result of binary search
+    if (flag == 0) {
+        printf("Element not found\n");
+    } else {
+        printf("Element %d found at position %d\n", x, mid + 1);  // Printing position (1-indexed)
     }
-}   
+    
+    // Space complexity: 4 bytes for integer * 2 (left and right) + 4 bytes per element in array
+    printf("\nSpace complexity: %d bytes\n", (4 * 2) + (4 * n)); 
+    
+    // Time complexity: O(log n) for binary search
+    printf("Time complexity: O(log n)\n");
+    
+    return 0;
+}
