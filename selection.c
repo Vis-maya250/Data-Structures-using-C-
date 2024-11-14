cdf82d754213b2f93f342bdff9363872d2e0a6ae
Vis_maya_ <vismayavinod250@gmail.com>
2024-11-14 06:29:08 +0530

    Selection.c

--- selection.c
+++ selection.c
@@ -1,58 +1,56 @@
-#include<stdio.h>
-void main()
-{
-       int n,A[100],i=0,j=0,small,temp,time=0;
-       time=time+3;
-       printf("enter the number of elements: ");
-       time++;
-       scanf("%d",&n);
-       time++;
-       printf("enter the elements: ");
-       time++;
-    for(i=0;i<n;i++)
-       {
-          scanf("%d",&A[i]);
-          time=time+2;
-        }
+#include <stdio.h>
+
+void main() {
+    int n, A[100], i = 0, j = 0, small, temp, time = 0;
+
+    time += 3;
+    printf("enter the number of elements: ");
+    time++;
+    scanf("%d", &n);
+    time++;
+    printf("enter the elements: ");
+    time++;
+    
+    for (i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+        time += 2;
+    }
+    
+    time++;
+    for (i = 0; i < n - 1; i++) {
         time++;
-    for(i=0;i<n;i++)
-        {
-           time++;
-           small=i;
-           j=j+1;
-           time=time+2;
-           while(j<n)
-             {
-               time++;
-            if(A[small]>A[j])
-              {
-                 small=j;
-                 j=j+1;
-                 time=time+3;
-               }
-            else
-               {
-                  j=i+1;
-                  time=time+2;
-                }
-                }
-       time++;
-       if(small!=i)
-         {
-                 temp=A[i];
-                 A[i]=A[small];
-                 time=time+3;
-          }
-          }
-          time=time+1;
-          printf("sorted array : \n");
-          for(i=0;i<n;i++)
-            {
-               printf("%d\t",A[i]);
-               time=time+2;
+        small = i;
+        j = i + 1;
+        time += 2;
+        
+        while (j < n) {
+            time++;
+            if (A[small] > A[j]) {
+                small = j;
+                time += 3;
             }
-           time++;
-           printf("\n space complexity=%d\n",28+(n*4));
-           time++;
-           printf("time complexity=%d",time++);
-}    
+            j++;
+            time++;
+        }
+        
+        time++;
+        if (small != i) {
+            temp = A[i];
+            A[i] = A[small];
+            A[small] = temp;
+            time += 3;
+        }
+    }
+    
+    time++;
+    printf("sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t", A[i]);
+        time += 2;
+    }
+    
+    time++;
+    printf("\nspace complexity = %d\n", 28 + (n * 4));
+    time++;
+    printf("time complexity = %d", time++);
+}
